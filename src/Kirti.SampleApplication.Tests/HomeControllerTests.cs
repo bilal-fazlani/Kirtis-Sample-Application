0d1b097c1c7a808e50cd87950e07b5ae6927a098
Bilal Fazlani <bfazlani@seek.com.au>
2016-05-03 09:20:09 +0530

    Add project files.

---
+++ src/Kirti.SampleApplication.Tests/HomeControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Kirti.SampleApplication.Controllers;
+using Microsoft.AspNet.Mvc;
+using Xunit;
+
+namespace Kirti.SampleApplication.Tests
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void CanReturnIndexView()
+        {
+            HomeController controller = new HomeController();
+
+            var response = controller.Index();
+
+            response.ExecuteResultAsync(new ActionContext());
+
+            response.Should().BeOfType<ViewResult>();
+        }
+    }
+}
---
+++ src/Kirti.SampleApplication.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Kirti.SampleApplication.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Seek Limited")]
+[assembly: AssemblyProduct("Kirti.SampleApplication.Tests")]
+[assembly: AssemblyCopyright("Copyright © Seek Limited 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e461a7a2-66e5-4cb8-9b1b-8ae193ddb726")]
